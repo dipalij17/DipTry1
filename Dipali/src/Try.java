2ec486fff4f0da10ce7b8e406aada73a9a87ad9f
hp <hp@LAPTOP-TB6Q99QE>
2021-11-21 20:31:48 +0530

    1st Commit

---
+++ Dipali/src/Try.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" My name is 'Dipali Jadhav.' ");
+	}
+
+}
---
+++ Dipali/src/dip/Try1.java
@@ -0,0 +1,11 @@
+package dip;
+
+public class Try1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" Trying to upload project on Git ");
+	}
+
+}
